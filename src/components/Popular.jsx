9d34811c1f80aa8357919363aac15b757b65977b
Chris Mittell <mittell@hotmail.co.uk>
2022-09-14 12:50:39 +0900

    fix: added missing Link

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/react-splide/css';
+import { Link } from 'react-router-dom';
 
 const Wrapper = styled.div`
 	margin: 4rem 0rem;
@@ -88,9 +89,11 @@ const Popular = () => {
 					{popular.map((recipe) => (
 						<SplideSlide key={recipe.id}>
 							<Card>
-								<p>{recipe.title}</p>
-								<img src={recipe.image} alt={recipe.title} />
-								<Gradient />
+								<Link to={`/recipe/${recipe.id}`}>
+									<p>{recipe.title}</p>
+									<img src={recipe.image} alt={recipe.title} />
+									<Gradient />
+								</Link>
 							</Card>
 						</SplideSlide>
 					))}
