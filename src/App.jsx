124a273dc6848fc050725b6017bc70b04a1308d3
Chris Mittell <mittell@hotmail.co.uk>
2022-09-14 10:10:36 +0900

    feat: added base components and logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Pages from './pages/Pages';
+
 function App() {
-  return (
-    <div >
-    
-    </div>
-  );
+	return (
+		<div>
+			<h1>App</h1>
+			<Pages />
+		</div>
+	);
 }
 
 export default App;
