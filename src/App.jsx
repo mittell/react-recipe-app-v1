c4c60b31e117b2f283f44ec2b9451905c3376096
Chris Mittell <mittell@hotmail.co.uk>
2022-09-14 10:38:17 +0900

    chore: added component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Category from './components/Category';
 import Pages from './pages/Pages';
 
 function App() {
 	return (
 		<div>
-			<h1>App</h1>
+			<Category />
 			<Pages />
 		</div>
 	);
