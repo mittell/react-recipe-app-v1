ad1306f3c39cce44b7a90cf4e941517c13e92380
Chris Mittell <mittell@hotmail.co.uk>
2022-09-14 10:38:06 +0900

    feat: added Category component

---
+++ src/components/Category.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { FaPizzaSlice, FaHamburger } from 'react-icons/fa';
+import { GiNoodles, GiChopsticks } from 'react-icons/gi';
+import styled from 'styled-components';
+
+const List = styled.div`
+	display: flex;
+	justify-content: center;
+	margin: 2rem 0rem;
+`;
+
+const Category = () => {
+	return (
+		<List>
+			<div>
+				<FaPizzaSlice />
+				<h4>Italian</h4>
+			</div>
+			<div>
+				<FaHamburger />
+				<h4>American</h4>
+			</div>
+			<div>
+				<GiNoodles />
+				<h4>Thai</h4>
+			</div>
+			<div>
+				<GiChopsticks />
+				<h4>Japanese</h4>
+			</div>
+		</List>
+	);
+};
+
+export default Category;
