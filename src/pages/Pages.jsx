589295ebe62430c50e97e2988efc77c5268e4228
Chris Mittell <mittell@hotmail.co.uk>
2022-09-14 12:15:34 +0900

    feat: added Search component, SearchResults page

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Cuisine from './Cuisine';
+import SearchResults from './SearchResults';
 
 const Pages = () => {
 	return (
 		<Routes>
 			<Route path='/' element={<Home />} />
 			<Route path='/cuisine/:type' element={<Cuisine />} />
+			<Route path='/search/:search' element={<SearchResults />} />
 		</Routes>
 	);
 };
---
+++ src/pages/SearchResults.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import { Link, useParams } from 'react-router-dom';
+
+const Grid = styled.div`
+	display: grid;
+	grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+	grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+	img {
+		width: 100%;
+		border-radius: 2rem;
+	}
+
+	a {
+		text-decoration: none;
+	}
+
+	h4 {
+		text-align: center;
+		padding: 1rem;
+	}
+`;
+
+const SearchResults = () => {
+	const [results, setResults] = useState([]);
+	let params = useParams();
+
+	useEffect(() => {
+		getResults(params.search);
+	}, [params.search]);
+
+	const getResults = async (search) => {
+		const api = await fetch(
+			`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&number=9&query=${search}`
+		);
+		const data = await api.json();
+		setResults(data.results);
+	};
+
+	return (
+		<Grid>
+			{results.map((item) => (
+				<Card key={item.id}>
+					<img src={item.image} alt={item.title} />
+					<h4>{item.title}</h4>
+				</Card>
+			))}
+		</Grid>
+	);
+};
+
+export default SearchResults;
